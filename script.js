0bd461b32aa2c31dfece190c4a67d7108e8faafc
Ahmed Hassan <ah2300487@gmail.com>
2023-12-18 00:19:10 +0200

    add functions

--- script.js
+++ script.js
@@ -1,21 +1,160 @@
 
+// variable >>>>>>
 const AlgorithmsButtons = document.querySelectorAll('input[name="algorithm"]');
 const TypeButtons = document.querySelectorAll('input[name="type"]');
 let key = document.querySelector(".special");
+let input1 = document.querySelector("#input1");
+let AlgorithmSelected;
+let TypeSelected;
+let typeA = document.querySelector('#typeA');
+let typeB = document.querySelector("#typeB")
 
+
+// what algorithm you are click  in 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 for (let i = 0; i < AlgorithmsButtons.length; i++) {
     AlgorithmsButtons[i].addEventListener('click', function (e) {
-        if(AlgorithmsButtons[i].value == "Affine"){
-            document.querySelector("#appearWhenSelectAffine").classList.replace('d-none','d-block');
-            document.querySelector('#key').classList.replace('d-block','d-none')
-        }else{
-            document.querySelector("#appearWhenSelectAffine").classList.replace('d-block','d-none');
-            document.querySelector('#key').classList.replace('d-none','d-block')
+        if (AlgorithmsButtons[i].value == "Affine") {
+            document.querySelector("#appearWhenSelectAffine").classList.replace('d-none', 'd-block');
+            document.querySelector('#key').classList.replace('d-block', 'd-none')
+        } else {
+            document.querySelector("#appearWhenSelectAffine").classList.replace('d-block', 'd-none');
+            document.querySelector('#key').classList.replace('d-none', 'd-block')
         }
+        AlgorithmSelected = AlgorithmsButtons[i].value;
     })
 }
+
+// what type you are click in
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>
 for (let i = 0; i < TypeButtons.length; i++) {
     TypeButtons[i].addEventListener('click', function (e) {
-        console.log(e.target.value)
+        TypeSelected = TypeButtons[i].value;
     })
 }
+
+// function convert (encription , decription )
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+document.querySelector("#convert").addEventListener('click', function () {
+    if (AlgorithmSelected != undefined && TypeSelected != undefined && key.value != '' && input1.value != '' && AlgorithmSelected != "Affine") {
+        // do all algorithm without affine
+        if (AlgorithmSelected == "Ceaser") {
+            //do ceaser
+            // console.log(input1.value)
+            // console.log(key.value)
+            // encription(input1.value, key.value)
+
+        } else {
+            //do viginere
+            console.log("viginer")
+        }
+    } else if (AlgorithmSelected != undefined && TypeSelected != undefined && input1.value != '' && AlgorithmSelected == "Affine" && typeA.value != "" && typeB.value != '') {
+        // do affine here
+        console.log("affine");
+    }
+})
+
+// 
+// /// Ceaser Algorithm
+// //>>>>>>>>>>>>>>>>>>>>>>
+
+// //1.Encrypt >>>>>>
+// function encrypt(text, key) {
+//     let result = '';
+//     for (let i = 0; i < text.length; i++) {
+//         let charCode = text.charCodeAt(i);
+//         if (charCode >= 65 && charCode <= 90) {
+//             result += String.fromCharCode((charCode - 65 + key) % 26 + 65);
+//         } else if (charCode >= 97 && charCode <= 122) {
+//             result += String.fromCharCode((charCode - 97 + key) % 26 + 97);
+//         } else {
+//             result += text.charAt(i);
+//         }
+//     }
+//     return result;
+// }
+
+// //2.Decrypt >>>>>>>>>>
+// function decrypt(text, key) {
+//     let result = '';
+//     for (let i = 0; i < text.length; i++) {
+//         let charCode = text.charCodeAt(i);
+//         if (charCode >= 65 && charCode <= 90) {
+//             result += String.fromCharCode((charCode - 65 - key + 26) % 26 + 65);
+//         } else if (charCode >= 97 && charCode <= 122) {
+//             result += String.fromCharCode((charCode - 97 - key + 26) % 26 + 97);
+//         } else {
+//             result += text.charAt(i);
+//         }
+//     }
+//     return result;
+// }
+
+// // take text input and the key from user
+// let text = prompt('Enter the text you want to encrypt/decrypt:');
+// let key = parseInt(prompt('Enter the key number:'));
+// let mode = prompt('Do you want to encrypt or decrypt?');
+
+// if (mode.toLowerCase() === 'encrypt') {
+//     let encryptedText = encrypt(text, key);
+//     console.log('Encrypted Text:', encryptedText);
+// } else if (mode.toLowerCase() === 'decrypt') {
+//     let decryptedText = decrypt(text, key);
+//     console.log('Decrypted Text:', decryptedText);
+// } else {
+//     console.log('Invalid mode selected. Please choose "encrypt" or "decrypt".');
+// }
+
+
+
+// let set = {
+//     'a': 0,
+//     'b': 1,
+//     'c': 2,
+//     'd': 3,
+//     'e': 4,
+//     'f': 5,
+//     'g': 6,
+//     'h': 7,
+//     'i': 8,
+//     'j': 9,
+//     'k': 10,
+//     'l': 11,
+//     'm': 12,
+//     'n': 13,
+//     'o': 14,
+//     'p': 15,
+//     'q': 16,
+//     'r': 17,
+//     's': 18,
+//     't': 19,
+//     'u': 20,
+//     'v': 21,
+//     'w': 22,
+//     'x': 23,
+//     'y': 24,
+//     'z': 25,
+// }
+
+
+// let n = 26;
+// let result = "";
+
+// let x = Object.keys(set)
+// console.log(x)
+// function encription(text, key) {
+//     for (let i = 0; i < text.length; i++) {
+
+//         for (let j = 0; j < x.length; j++) {
+//             if ((set[text[i]] + key) % n == set[x[j]]) {
+//                 result += x[j]
+
+//             }
+//         }
+//     }
+//     console.log(result)
+// }
+
+
