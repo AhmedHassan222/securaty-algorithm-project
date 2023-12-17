53d5c072053ff805018ff638f1387d2359bff10e
Ahmed Hassan <ah2300487@gmail.com>
2023-12-17 22:40:42 +0200

    update UI

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ let key = document.querySelector(".special");
 for (let i = 0; i < AlgorithmsButtons.length; i++) {
     AlgorithmsButtons[i].addEventListener('click', function (e) {
         if(AlgorithmsButtons[i].value == "Affine"){
-            document.querySelector("#appearWhenSelectAffine").classList.replace('d-none','d-flex');
+            document.querySelector("#appearWhenSelectAffine").classList.replace('d-none','d-block');
             document.querySelector('#key').classList.replace('d-block','d-none')
         }else{
-            document.querySelector("#appearWhenSelectAffine").classList.replace('d-flex','d-none');
+            document.querySelector("#appearWhenSelectAffine").classList.replace('d-block','d-none');
             document.querySelector('#key').classList.replace('d-none','d-block')
         }
     })
